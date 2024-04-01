d58f13b4dbe1ef038e5bba8e86fd7cbd888f8507
abhilash-patra <86566879+abhilash-patra@users.noreply.github.com>
2024-04-01 16:49:31 -0400

    Add files via upload

--- qchem_workflow/output_analysis/charge_out/geometry/transfer_geom.py
+++ qchem_workflow/output_analysis/charge_out/geometry/transfer_geom.py
@@ -1,1 +1,44 @@
+from pymatgen.io.qchem.inputs import QCInput
+from pymatgen.io.qchem.outputs import QCOutput
+
+import glob
+
+new_rems = {
+"BASIS":"6-311g**",
+"CIS_STATE_DERIV":"1",
+"JOB_TYPE":"SP",
+"METHOD":"wb97xd",
+"CIS_N_ROOTS":"10",
+"CIS_SINGLETS":"1",
+"CIS_TRIPLETS":"0",
+"RPA":"2",
+"POP_MULLIKEN":"-1",
+"SOLVENT_METHOD":"PCM",
+"SCF_CONVERGENCE":"8",
+"SYMMETRY":"FALSE",
+"SYMMETRY_IGNORE":"1",
+"THRESH":"14"
+}
+
+new_pcms = {
+"HEAVYPOINTS":"590",
+"METHOD":"SWIG",
+"RADII":"BONDI",
+"SOLVER":"INVERSION",
+"THEORY":"CPCM"
+}
+
+new_solvents = {
+"DIELECTRIC":"2.03",
+"OPTICALDIELECTRIC":"2.01"
+}
+
+
+for inp in glob.glob('*.out'):
+    label = inp[0:-4]
+    output = QCOutput(filename = inp)
+    opt_geom = output.data['molecule_from_last_geometry']
+    NewInput = QCInput(molecule = opt_geom, rem = new_rems, pcm = new_pcms, solvent = new_solvents)
+    NewInput.write_file(label +'.inp')
+
 
